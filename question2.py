42c86e9f6890403da3336da4ace91686260587b8
rjones136 <rjone136@smartweb.augustatech.edu>
2026-09-03 14:48:10 -0400

    Refactor centimeter to mickey conversion script
    Updated comments for clarity and fixed output message.

--- question2.py
+++ question2.py
@@ -1,6 +1,13 @@
-# Homework 2
-#Question 2 - Centimeters to Mickeys
-centimeters = 1 
+#problem:
+#convert centimeters to mickeys
+#input: distance in centimeters
+#output: distance in mickeys
+#process: convert centimeters to millimeters, then multiply by 16 
+
+print("Welcome to the Mickey converter")
+
+centimeters = float(input("Enter distance in centimeters: "))
 millimeters = centimeters * 10
 mickeys = millimeters * 16
-print("Distance in Mickeys:", mickeys)
+
+print("Your distance in mickeys is", mickeys)
