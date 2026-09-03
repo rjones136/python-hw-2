b27d6c0b29f866b0d1653950c0fe94d88c828c01
rjones136 <rjone136@smartweb.augustatech.edu>
2026-09-03 15:00:00 -0400

    Add Wheaton converter for follower count

---
+++ question3.py
@@ -0,0 +1,12 @@
+#problem:
+#convert x followers to Wheatons
+#input: number of x followers
+#output: number of wheatons
+#process: divide the number of followers by 500000 
+
+print("Welcome to the Wheaton converter")
+
+followers = float(input("Enter number of x followers: "))
+wheatons = followers / 500000
+
+print("Your number of Wheatons is", wheatons)
